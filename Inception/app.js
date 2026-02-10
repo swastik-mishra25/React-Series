90000fece415e9eadcc45ab8613f5b202e16a611
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2026-02-11 00:41:52 +0530

    Just Updated the part 4 of the First Video

--- Inception/app.js
+++ Inception/app.js
@@ -1,3 +1,36 @@
+//How to create this type of structure of HTML in react
+/**
+ *
+ *<div id="parent">
+ *      <div id="child">
+ *           <h1></h1>
+ *      </div>
+ *</div> 
+ * 
+ * 
+ */
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        React.createElement("h1", {}, "I'm an h1 tag")
+    )
+);
+
+
+
+
+
+
+
+
+
+
+
+
 const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!");
 console.log(heading); // It will return an object
 const root = ReactDOM.createRoot(document.getElementById("root"));
