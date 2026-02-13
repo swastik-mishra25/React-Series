f4ec30737f7acd147cbb170187d22438089307f0
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2026-02-13 23:41:13 +0530

    Completed the part v

--- Inception/app.js
+++ Inception/app.js
@@ -1,16 +1,28 @@
 //How to create this type of structure of HTML in react
 /**
- *
+ * Nested Structure
+ * 
  *<div id="parent">
  *      <div id="child">
  *           <h1></h1>
  *      </div>
  *</div> 
  * 
+ * React.createElement eod is creating an object....which is a HTML understood by the browser...
+ *
+ * Sibling Structure
  * 
+ * <div id="parent">
+ *      <div id="child">
+ *           <h1>I'm h1 tag</h1>
+ *           <h2>I'm h1 tag</h1>
+ *      </div>
+ *</div> 
  */
 
-const parent = React.createElement( //parent is an object
+// Nested Structure Code
+
+/*const parent = React.createElement( //parent is an object, react element
     "div",
     { id: "parent" },
     React.createElement(
@@ -19,9 +31,57 @@ const parent = React.createElement( //parent is an object
         React.createElement("h1", {}, "I'm an h1 tag")
     )
 );
-console.log(parent);
+console.log(parent); //Object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(parent); */
+
+// Sibling Structure
+
+// If you want to make the sibling structure from the nested structure....make the 3rd arguement in the array form...
+
+const parent = React.createElement( //parent is an object, react element
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h1 tag")
+        ])
+); //Array of children
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+// One parent multiple child
+/*
+*<div id = "parent">
+        <div id = "child">
+            <h1>I'm h1 tag </h1>
+            <h2>I'm h1 tag </h2>
+        </div>
+<div id = "child2">
+            <h1>I'm h1 tag </h1>
+            <h2>I'm h1 tag </h2>
+        </div>
+    </div>
+
+*/
+
+const parentone = React.createElement("div", { id: "parent"}, [
+    React.createElement("div", { id: "child"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+]);
+console.log(parentone);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parentone);
+// Due to above complexity...
 
 
 
@@ -33,7 +93,7 @@ root.render(parent);
 
 
 
-
+// Basic Program
 // const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!");
 // console.log(heading); // It will return an object
 // const root = ReactDOM.createRoot(document.getElementById("root"));
