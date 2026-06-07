e83ed1204a09c591eb186f2b64933aa55547d0fa
swastik-mishra25 <128520442+swastik-mishra25@users.noreply.github.com>
2026-06-08 01:43:02 +0530

    got to know about jsx

--- Laying the foundation/app.js
+++ Laying the foundation/app.js
@@ -1,5 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
+// React.createElement => Object => HTMLElement(render)
 
+// It is made up of core React !!
+
+// React Element made up of core React
+
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Namaste React"
+);
+
+// JSX - HTML-like or XML-like syntax
+
+// Creating H1 tag using jsx
+
+// React Element made up of jsx..
+
+const jsxHeading = <h1>Namaste React using JSX</h1>
+
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
