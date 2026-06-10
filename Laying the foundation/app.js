72c42f5239c7ab5c7f18bca9ace032c9c3e97a0e
swastik-mishra25 <128520442+swastik-mishra25@users.noreply.github.com>
2026-06-10 20:50:53 +0530

    Completion of Component Composition

--- Laying the foundation/app.js
+++ Laying the foundation/app.js
@@ -7,11 +7,7 @@ import ReactDOM from "react-dom/client";
 
 // React Element made up of core React
 
-const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Namaste React"
-);
+const heading1 = React.createElement("h1", { id: "heading" }, "Namaste React");
 
 // JSX - HTML-like or XML-like syntax
 
@@ -23,26 +19,67 @@ const heading = React.createElement(
 
 //JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
 
-const jsxHeading = <h1 className="head">Namaste React using JSX</h1>
+const jsxHeading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
 
 console.log(jsxHeading);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
 
 // Part_04_Begins
 
 // React Element
 
-const heading = (
-<h1 className="head">
-    Namaste React using JSX
-    </h1>
-); 
+const heading = <h1 className="head">Namaste React using JSX</h1>;
 
 // React Component
+// Class Based Component - OLD
+// Functional Component - NEW
 
+// React Functional Component :- It is just a javascript function
 
+const HeadingComponent = () => {
+  return <h1 className="heading"> Namaste React Functional Component </h1>;
+};
 
+const HeadingComponent2 = () => (
+  <h1 className="heading"> Namaste React Functional Component </h1>
+);
+// In Industry developer's write the React Functional Component like this !!
 
+// Nested JSX
 
+const HeadingComponent3 = () => (
+  <div id="container">
+    <h1 className="heading"> Namaste React Functional Component </h1>
+  </div>
+);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<HeadingComponent />);
+
+// Injection of one code into the another one
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
+
+// Component Compositions
+
+const Heading_Component = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Heading_Component />);
