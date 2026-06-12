8521d6abcadb57bc9bc69cb32bd0b1667cfd9331
swastik-mishra25 <128520442+swastik-mishra25@users.noreply.github.com>
2026-06-13 00:34:41 +0530

    Completion of Laying the foundation

--- Laying the foundation/app.js
+++ Laying the foundation/app.js
@@ -15,9 +15,9 @@ const heading1 = React.createElement("h1", { id: "heading" }, "Namaste React");
 
 // Creating H1 tag using jsx
 
-// React Element made up of jsx..
-
 //JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+// React Element made up of jsx..
 
 const jsxHeading = (
   <h1 className="head" tabIndex="5">
@@ -78,7 +78,7 @@ const Title = () => (
 const Heading_Component = () => (
   <div id="container">
     <Title />
-    <h1 className="heading">Namaste React Functional Component</h1>
+    <h1 className="heading">Namaste React Functional Component </h1>
   </div>
 );
 
@@ -90,10 +90,52 @@ const Heading_Component = () => (
 
 const Footer = function (){
     return (
-        <footer className="foot"> Created by Swastik Mishra</footer>
+        <footer className="foot"> Created by Swastik Mishra </footer>
     );
 };
 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<Footer />);
+
+// But arrow function is the newer function and industry wise standard..  
+
+// JavaScript Expressions in JSX
+
+const name = "Swastik Mishra" ;
+
+const Jsxexpression = () => (
+  <div id="container">
+    <h2>{name}</h2>
+    <h1 className="heading"></h1>
+  </div>
+);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<Jsxexpression />);
+
+// Putting react element inside the react component !!
+
+// ReactElement made up of JSX !!
+
+const title = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
+
+const H_C = () => (
+  <div id="container">
+    {title}
+    <h1 className="heading">Namaste React Functional Component </h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Footer />);
+root.render(<H_C />);
+
+// Putting react element inside a react element is same as putting react element inside a react component !! 
+
+
