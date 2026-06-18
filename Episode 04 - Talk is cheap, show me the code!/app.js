47a3b7225b1ff12171f3581db344ca52dc3f7c3f
swastik-mishra25 <128520442+swastik-mishra25@users.noreply.github.com>
2026-06-18 17:04:49 +0530

    Mid_way

--- Episode 04 - Talk is cheap, show me the code!/app.js
+++ Episode 04 - Talk is cheap, show me the code!/app.js
@@ -1,141 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Components my app can have !!
+/*
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - RestaurantContainer
+ * - RestaurantCard
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
 
-// React.createElement => Object => HTMLElement(render)
-
-// It is made up of core React !!
-
-// React Element made up of core React
-
-const heading1 = React.createElement("h1", { id: "heading" }, "Namaste React");
-
-// JSX - HTML-like or XML-like syntax
-
-// JSX (transpiled before it reaches the JS) - PARCEL - Babel
-
-// Creating H1 tag using jsx
-
-//JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-// React Element made up of jsx..
-
-const jsxHeading = (
-  <h1 className="head" tabIndex="5">
-    Namaste React using JSX
-  </h1>
-);
-
-console.log(jsxHeading);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(jsxHeading);
-
-// Part_04_Begins
-
-// React Element
-
-const heading = <h1 className="head">Namaste React using JSX</h1>;
-
-// React Component
-// Class Based Component - OLD
-// Functional Component - NEW
-
-// React Functional Component :- It is just a javascript function
-
-const HeadingComponent = () => {
-  return <h1 className="heading"> Namaste React Functional Component </h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://imgs.search.brave.com/e8Y3wq81MufnWDEqhsp3ywx3wrarM2yz9tjmbEO2Z-g/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/bG9nb2Rlc2lnbi5u/ZXQvbG9nby1uZXcv/aXRhbGlhbi1yZXN0/YXVyYW50LXN5bWJv/bC1vZi1jaGVmLWhv/bGRpbmctY2xvY2hl/LTc2ODhsZC5wbmc_/bndtPTEmbndzPTEm/aW5kdXN0cnk9Zm9v/ZCZ0eHRfa2V5d29y/ZD1BbGw"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const HeadingComponent2 = () => (
-  <h1 className="heading"> Namaste React Functional Component </h1>
-);
-// In Industry developer's write the React Functional Component like this !!
-
-// Nested JSX
-
-const HeadingComponent3 = () => (
-  <div id="container">
-    <h1 className="heading"> Namaste React Functional Component </h1>
-  </div>
-);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(<HeadingComponent />);
-
-// Injection of one code into the another one
-
-// Component Compositions
-
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    Namaste React using JSX
-  </h1>
-);
-
-
-
-const Heading_Component = () => (
-  <div id="container">
-    <Title />
-    <h1 className="heading">Namaste React Functional Component </h1>
-  </div>
-);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(<Heading_Component />);
-
-// Writing the Component Using the Normal function Without the Arrow One
-
-const Footer = function (){
-    return (
-        <footer className="foot"> Created by Swastik Mishra </footer>
-    );
+const AppLayout = () => {
+  return <div className="app">
+  {/* // Header // Body // Footer */}
+  <Header/>
+  </div>;
 };
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(<Footer />);
-
-// But arrow function is the newer function and industry wise standard..  
-
-// JavaScript Expressions in JSX
-
-const name = "Swastik Mishra" ;
-
-const Jsxexpression = () => (
-  <div id="container">
-    <h2>{name}</h2>
-    <h1 className="heading"></h1>
-  </div>
-);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(<Jsxexpression />);
-
-// Putting react element inside the react component !!
-
-// ReactElement made up of JSX !!
-
-const title = (
-  <h1 className="head" tabIndex="5">
-    Namaste React using JSX
-  </h1>
-);
-
-const H_C = () => (
-  <div id="container">
-    {title}
-    <h1 className="heading">Namaste React Functional Component </h1>
-  </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<H_C />);
-
-// Putting react element inside a react element is same as putting react element inside a react component !! 
-
-
+root.render(<AppLayout />);
