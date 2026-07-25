31314ae39ab8e65634de17c4f66797fab7ea71a0
swastik-mishra25 <128520442+swastik-mishra25@users.noreply.github.com>
2026-07-25 23:20:52 +0530

    Starting of this Episode

---
+++ Episode 06 - Exploring the world/src/components/Body.js
@@ -0,0 +1,153 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState } from "react";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  // Use of Hooks
+
+  // Local State Variable - Super powerful variable
+
+  const [listOfRestaurants, setListOfRestaurant] = useState(
+  //   [
+  //   {
+  //     data: {
+  //       id: "334476",
+  //       name: "KFC",
+  //       cloudinaryImageId: "dominospizza123",
+  //       cuisines: ["Pizza", "Italian", "Fast Food"],
+  //       costForTwo: 60000,
+  //       deliveryTime: 30,
+  //       avgRating: 3.8,
+  //     },
+  //   },
+  //   {
+  //     data: {
+  //       id: "334477",
+  //       name: "Domino's Pizza",
+  //       cloudinaryImageId: "dominospizza123",
+  //       cuisines: ["Pizza", "Italian", "Fast Food"],
+  //       costForTwo: 60000,
+  //       deliveryTime: 30,
+  //       avgRating: 4.4,
+  //     },
+  //   },
+  //   {
+  //     data: {
+  //       id: "334478",
+  //       name: "MCD",
+  //       cloudinaryImageId: "dominospizza123",
+  //       cuisines: ["Pizza", "Italian", "Fast Food"],
+  //       costForTwo: 60000,
+  //       deliveryTime: 30,
+  //       avgRating: 4.1,
+  //     },
+  //   },
+  // ]
+  resList
+);
+
+  // NORMAL JS VARIABLE
+  // let listOfRestaurants;
+
+  // Normal Js Variable
+  // let listOfRestaurants = [
+  //   {
+  //     data: {
+  //       id: "334476",
+  //       name: "KFC",
+  //       cloudinaryImageId: "dominospizza123",
+  //       cuisines: ["Pizza", "Italian", "Fast Food"],
+  //       costForTwo: 60000,
+  //       deliveryTime: 30,
+  //       avgRating: 3.8,
+  //     },
+  //   },
+  //   {
+  //     data: {
+  //       id: "334477",
+  //       name: "Domino's Pizza",
+  //       cloudinaryImageId: "dominospizza123",
+  //       cuisines: ["Pizza", "Italian", "Fast Food"],
+  //       costForTwo: 60000,
+  //       deliveryTime: 30,
+  //       avgRating: 4.4,
+  //     },
+  //   },
+  //   {
+  //     data: {
+  //       id: "334478",
+  //       name: "MCD",
+  //       cloudinaryImageId: "dominospizza123",
+  //       cuisines: ["Pizza", "Italian", "Fast Food"],
+  //       costForTwo: 60000,
+  //       deliveryTime: 30,
+  //       avgRating: 4.1,
+  //     },
+  //   },
+  // ];
+
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+
+            // For the usestate hook
+
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4,
+            );
+            setListOfRestaurant(filteredList);
+
+            // For checking whether the onclick is working is not !!
+
+            // console.log("Button Clicked");
+
+            // Filter logic here for normal javascript
+
+            // listOfRestaurants = listOfRestaurants.filter(
+            //   (res) => res.data.avgRating > 4,
+            // );
+            // console.log(listOfRestaurants);
+
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {/* <RestaurantCard
+         resName="Sai Fast Food"
+          cuisine="Biryani, North Indian, Asian"
+        /> */}
+        {/* <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" /> */}
+
+        {/* FOR KFC ONCE
+        <RestaurantCard resData={resList} /> */}
+        {/* FOR MANY RESTAURANT
+        <RestaurantCard resData={resList[0]} />
+        <RestaurantCard resData={resList[1]} />
+        <RestaurantCard resData={resList[2]} />
+        <RestaurantCard resData={resList[3]} />
+        <RestaurantCard resData={resList[4]} />
+        <RestaurantCard resData={resList[5]} />
+        <RestaurantCard resData={resList[6]} />
+        <RestaurantCard resData={resList[7]} />
+        <RestaurantCard resData={resList[8]} />
+        */}
+
+        {/* THE BEST PRACTICE  */}
+
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
+
---
+++ Episode 06 - Exploring the world/src/utils/constants.js
@@ -0,0 +1,7 @@
+export const CDN_URL = 
+"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+"https://imgs.search.brave.com/e8Y3wq81MufnWDEqhsp3ywx3wrarM2yz9tjmbEO2Z-g/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/bG9nb2Rlc2lnbi5u/ZXQvbG9nby1uZXcv/aXRhbGlhbi1yZXN0/YXVyYW50LXN5bWJv/bC1vZi1jaGVmLWhv/bGRpbmctY2xvY2hl/LTc2ODhsZC5wbmc_/bndtPTEmbndzPTEm/aW5kdXN0cnk9Zm9v/ZCZ0eHRfa2V5d29y/ZD1BbGw";
+
+
