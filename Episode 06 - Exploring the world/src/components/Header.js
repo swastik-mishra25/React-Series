7cc9ea49cc337c0775453073fa5b1d59194e7a84
swastik-mishra25 <128520442+swastik-mishra25@users.noreply.github.com>
2026-08-11 03:47:39 +0530

    Ep.06 Part.05...Completed

--- Episode 06 - Exploring the world/src/components/Header.js
+++ Episode 06 - Exploring the world/src/components/Header.js
@@ -1,7 +1,18 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 export const Header = () => {
-  let btnName = "Login";
+  // When used as a variable
+
+  // let btnName = "Login";
+
+  // When used as a state variable
+
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  // It is basically a proof to showcase that the react will re-render the whole header component
+  console.log("Header render");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,14 +24,42 @@ export const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
-          <button
+
+          {/* When used as a variable */}
+
+          {/* <button
             className="login"
             onClick={() => {
               btnName = "Logout";
               console.log(btnName);
             }}
+          > */}
+
+          {/* {btnName} */}
+
+          {/* When used as a state variable */}
+
+          {/* Here only one thing is happening Login -> Logout */}
+
+          {/* <button
+            className="login"
+            onClick={() => {
+              setBtnNameReact("Logout");
+              console.log(btnNameReact);
+            }}
           >
-            {btnName}
+            {btnNameReact}
+          </button> */}
+
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
           </button>
         </ul>
       </div>
